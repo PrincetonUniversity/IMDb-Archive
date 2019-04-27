8b6b000b8d22418f5e5b961826b572bdcc3fc0cd
Bo-Yuan Huang <byhuang1992@gmail.com>
2019-04-27 19:11:44 +0000

    run script and wrapper code for hw-cbmd comparison

---
+++ accls/AES/AES-RTL-C/HwCbmc/src/run.sh
@@ -0,0 +1,2 @@
+time hw-cbmc aes_128.v --module aes_128 bug.c --bound 22 --unwind 30 > ../logs/bug.log
+time hw-cbmc aes_128.v --module aes_128 wrong_map.c --bound 22 --unwind 25 --no-unwinding-assertions > ../logs/sat.log
