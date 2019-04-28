1cdbc8af7c3cfefea10381997131a12bda3c0493
Bo-Yuan Huang <byhuang1992@gmail.com>
2019-04-28 02:06:13 +0000

    Trap exit code

--- scripts/ci/aes-hwcbmc.sh
+++ scripts/ci/aes-hwcbmc.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 export AES_HWCBMC_DIR=$TRAVIS_BUILD_DIR/accls/AES/AES-RTL-C/HwCbmc
+export AES_HWCBMC_DIR=$(pwd)/accls/AES/AES-RTL-C/HwCbmc
 cd $AES_HWCBMC_DIR/src
-../hw-cbmc aes_128.v --module aes_128 bug.c --bound 5 --unwind 30
+
+scratch=$(mktemp -t tmp.XXXXXXXXXX)
+
+../hw-cbmc aes_128.v --module aes_128 bug.c --bound 5 --unwind 30 > $scratch
+
+function finish {
+  echo "finish"
+}
+
+trap finish EXIT
