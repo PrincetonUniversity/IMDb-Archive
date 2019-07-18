5a09ecb1199976fdc061f757404a0c6f31495667
Bo-Yuan Huang <byhuang1992@gmail.com>
2019-07-18 12:09:06 -0700

    Export ILA portable of GB high

--- accls/GB-Halide/ila_spec_a/gb_hier.py
+++ accls/GB-Halide/ila_spec_a/gb_hier.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import ila
 
 from gb_arch import GBArch
@@ -7,40 +7,38 @@ from gb_nxt_wr0 import WRU0
 from gb_nxt_wr1 import WRU1
 from gb_rdi import defNext as rdDefNext
 
-def defUSts (gb):
+
+def defUSts(gb):
     m = gb.abst
 
-    gb.pre_pix      = m.reg ('pre_pix', gb.DATA_SIZE)
-    gb.pre_pix_nxt  = gb.pre_pix
+    gb.pre_pix = m.reg('pre_pix', gb.DATA_SIZE)
+    gb.pre_pix_nxt = gb.pre_pix
 
-    gb.st_ready     = m.reg ('st_ready', 1)
+    gb.st_ready = m.reg('st_ready', 1)
     gb.st_ready_nxt = gb.st_ready
 
-    gb.proc_in      = m.reg ('proc_in', gb.slice_size * gb.stencil_size)
-    gb.proc_in_nxt  = gb.proc_in
-    
+    gb.proc_in = m.reg('proc_in', gb.slice_size * gb.stencil_size)
+    gb.proc_in_nxt = gb.proc_in
+
 # Define next state function for each instruction/child-instruction
-def defNext (gb):
-    WRI (gb)
-    WRU0 (gb)
-    WRU1 (gb)
+
+
+def defNext(gb):
+    WRI(gb)
+    WRU0(gb)
+    WRU1(gb)
 
 # Connect next state function to the abstraction
-def setNext (gb):
-    gb.setNext ()
-    
+
+
+def setNext(gb):
+    gb.setNext()
+
     m = gb.abst
-    m.set_next ('proc_in', gb.proc_in_nxt)
-    m.set_next ('pre_pix', gb.pre_pix_nxt)
-    m.set_next ('st_ready', gb.st_ready_nxt)
+    m.set_next('proc_in', gb.proc_in_nxt)
+    m.set_next('pre_pix', gb.pre_pix_nxt)
+    m.set_next('st_ready', gb.st_ready_nxt)
 
-if __name__ == '__main__':
-    gb = GBArch ()
-
-    defUSts (gb)
-    defNext (gb)
-    rdDefNext (gb)
-    setNext (gb)
 
 def ExportHierIla():
 
@@ -48,14 +46,17 @@ def ExportHierIla():
     if not os.path.exists(archive_dir):
         os.makedirs(archive_dir)
 
-    gb = GBArch ()
+    gb = GBArch()
 
-    defUSts (gb)
-    defNext (gb)
-    rdDefNext (gb)
-    setNext (gb)
+    defUSts(gb)
+    defNext(gb)
+    rdDefNext(gb)
+    setNext(gb)
+
+    gb.setDecode()
 
     gb.exportModel(archive_dir + '/gb_high.abst')
+
 
 if __name__ == '__main__':
     ExportHierIla()
