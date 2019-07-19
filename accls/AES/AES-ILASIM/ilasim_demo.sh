d17d637a0f7d91196faf53e8f4701d6839f9ece3
yuex1994 <yuexthu@gmail.com>
2019-07-19 15:59:11 +0000

    store the demo obj inside IMDb rather than the user's home directory

--- accls/AES/AES-ILASIM/ilasim_demo.sh
+++ accls/AES/AES-ILASIM/ilasim_demo.sh
@@ -3,18 +3,18 @@ ILANG_PATH=~/ILAng/
 IMDB_PATH=~/IMDb/
 
 DEMO_MODEL_PATH=$IMDB_PATH/accls/AES/AES-ILASIM/
-mkdir ~/ila_obj/
+mkdir $ILANG_PATH/ila_obj/
 cd $ILANG_PATH
 mkdir build 
 cd build
-cmake .. -DILANG_INSTALL_DEV=on -DCMAKE_INSTALL_PREFIX=~/ila_obj/
+cmake .. -DILANG_INSTALL_DEV=on -DCMAKE_INSTALL_PREFIX=$ILANG_PATH/ila_obj/
 make -j
 make install
 
 cd $DEMO_MODEL_PATH
 mkdir build 
 cd build
-cmake .. -DCMAKE_PREFIX_PATH=~/ila_obj/
+cmake .. -DCMAKE_PREFIX_PATH=$ILANG_PATH/ila_obj/
 make
 mkdir co_sim_model/
 ./AES_ILASIM ./co_sim_model/ $SYSTEMC_DIR
