8ce330cbe9c39e7aaf1e5c7505be3c4e757c530e
Huaixi Lu <51335367+HuaixiLu@users.noreply.github.com>
2019-10-07 18:02:33 -0400

    Revert "Revert "Merge branch 'nibbler' of https://github.com/PrincetonUniversity/IMDb into nibbler""
    This reverts commit f07beb96c011d3719ff0b6c97fac568a73cb96bf.

--- cores/Nibbler/RISC-V-ILA/verilog/param-CoreDpathAlu.v
+++ cores/Nibbler/RISC-V-ILA/verilog/param-CoreDpathAlu.v
@@ -9,9 +9,6 @@
 //-------------------------------------------------------------------------
 
 module  param_CoreDpathAlu
-// #(
-//   parameter P_NBITS = 4
-// )
 (
   input [3:0] in_a,
   input [3:0] in_b,
