fb8ee3580cc342b972431ec10ac4af46037df092
Bo-Yuan Huang <byhuang1992@gmail.com>
2019-10-31 11:53:55 -0400

    revert format change

--- cores/Nibbler/RISC-V-ILA/include/riscvIla.hpp
+++ cores/Nibbler/RISC-V-ILA/include/riscvIla.hpp
@@ -47,27 +47,27 @@ private:
   ExprRef csr_index;
 
 protected:
-  ExprRef indexIntoGPR(const ExprRef &idxBits);
-  void UpdateGPR(InstrRef &inst, const ExprRef &idxBits, const ExprRef &val);
+  ExprRef indexIntoGPR(const ExprRef& idxBits);
+  void UpdateGPR(InstrRef& inst, const ExprRef& idxBits, const ExprRef& val);
 
   ExprRef bv(NumericType val) { return BvConst(val, XLEN); }
-  ExprRef zext(const ExprRef &v) { return ZExt(v, XLEN); }
-  ExprRef sext(const ExprRef &v) { return SExt(v, XLEN); }
+  ExprRef zext(const ExprRef& v) { return ZExt(v, XLEN); }
+  ExprRef sext(const ExprRef& v) { return SExt(v, XLEN); }
 
-  ExprRef getSlice(const ExprRef &word, const ExprRef &lowBits, int width,
+  ExprRef getSlice(const ExprRef& word, const ExprRef& lowBits, int width,
                    bool unSigned);
-  ExprRef CombineSlices(const ExprRef &word, const ExprRef &lowBits, int width,
-                        const ExprRef &old);
+  ExprRef CombineSlices(const ExprRef& word, const ExprRef& lowBits, int width,
+                        const ExprRef& old);
 
   // privileged model will overload these to insert their address translation
-  virtual ExprRef FetchFromMem(const ExprRef &m, const ExprRef &addr) {
+  virtual ExprRef FetchFromMem(const ExprRef& m, const ExprRef& addr) {
     return Load(m, addr);
   }
-  virtual ExprRef LoadFromMem(const ExprRef &m, const ExprRef &addr) {
+  virtual ExprRef LoadFromMem(const ExprRef& m, const ExprRef& addr) {
     return Load(m, addr);
   }
-  virtual ExprRef StoreToMem(const ExprRef &m, const ExprRef &addr,
-                             const ExprRef &data) {
+  virtual ExprRef StoreToMem(const ExprRef& m, const ExprRef& addr,
+                             const ExprRef& data) {
     return Store(m, addr, data);
   }
 
